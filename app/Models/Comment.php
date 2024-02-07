5d40c5265e9a9e8bbc90dd6bd8bd7603f50b97fa
Heruvim6w <heruvim.6w@gmail.com>
2024-02-07 20:58:49 +0300

    Создаёт ресурс комментариев

---
+++ app/Models/Comment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int    $id
+ * @property string $content
+ * @property int    $user_id
+ * @property int    $post_id
+ * @property string $crated_at
+ * @property string $updated_at
+ */
+class Comment extends Model
+{
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
